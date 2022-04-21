0eb6da46567e1f0edc70e52d3c95f66922e28f80
JuanF20 <73490720+JuanF20@users.noreply.github.com>
2022-04-21 14:26:17 +0200

    Parte JuanFb y Jhonatan QT

--- src/entity/ArtistSchema.js
+++ src/entity/ArtistSchema.js
@@ -11,16 +11,30 @@ module.exports = new EntitySchema( {
             type:'int',
             generated:true
         },
-        user_name:{
+        name:{
             type:"varchar",
         },
-        song_date:{
-            type:"date",
+        last_name:{
+            type:"varchar",
         },
         
-        password:{
+        artist_name:{
+            type:"varchar",
+        },
+
+        gender_music:{
+            type:"varchar",
+        },
+
+        birth_place:{
+            type:"varchar",
+        },
+
+        description:{
             type:"varchar",
         }
+
+
 
     }
 })
